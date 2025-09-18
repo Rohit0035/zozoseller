a1dda6d6199eb156c1e38f365fc097c20dc6ab81
narendra lohar <narendraazsm@gmail.com>
2025-09-18 10:21:35 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/zozokart-seller">
       <ToastifyNotification />
       <App />
     </BrowserRouter>
